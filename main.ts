871f842fb5a1064913af035df1d143e783711c28
PhilippAusHeidelberg <110686148+PhilippAusHeidelberg@users.noreply.github.com>
2022-12-22 13:54:39 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,51 @@
+input.onGesture(Gesture.TiltRight, function () {
+	
+})
+input.onGesture(Gesture.LogoUp, function () {
+	
+})
+input.onGesture(Gesture.EightG, function () {
+	
+})
+input.onGesture(Gesture.ScreenDown, function () {
+	
+})
+input.onGesture(Gesture.FreeFall, function () {
+	
+})
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+	
+})
+input.onGesture(Gesture.TiltLeft, function () {
+	
+})
+input.onGesture(Gesture.SixG, function () {
+	
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+	
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onGesture(Gesture.LogoDown, function () {
+	
+})
+input.onGesture(Gesture.ScreenUp, function () {
+	
+})
+input.onGesture(Gesture.ThreeG, function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
